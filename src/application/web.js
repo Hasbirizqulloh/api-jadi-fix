0429e0055852b0dc8a48e634167c3b39f9ed0516
Hasbi Rizqulloh <hasbirzq08@gmail.com>
2023-12-21 18:06:04 +0700

    tambah cors

--- src/application/web.js
+++ src/application/web.js
@@ -6,7 +6,12 @@ import cors from 'cors';
 
 export const web = express();
 
-web.use(cors());
+const corsOptions = {
+  origin: 'https://pelma-uas-frontend.vercel.app',
+  credentials: true, // Izinkan kredensial
+};
+
+web.use(cors(corsOptions));
 web.use(express.json());
 web.use(publicRouter);
 web.use(userRouter);
